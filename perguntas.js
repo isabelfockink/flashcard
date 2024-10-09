ff43eec09776767d12f8633c8f5ff401c95c821f
isabelfockink <159458237+isabelfockink@users.noreply.github.com>
2024-10-09 11:21:32 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,62 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que e robotica',
+    'E a ciencia que estuda a computacao e planejamento dos robos'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil',
+    'A capital do Brasil e Brasilia'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual o maior animal terrestre',
+    'O maior animal terrestre e o Elefante'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é Bye`
 )
+
+criaCartao(
+    'Programação',
+    'Qual a Fórmula da Água?',
+    'A Fórmula da Água é H20'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?,
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'Qual o maior animal do planeta?',
+    'O maior animal do planeta é a  Baleia-Azul'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz verde em ingles',
+    'Verde em ingles é Green'
+
+)criaCartao(
+    'Programação',
+    'Qual o maior peixe do mundo?',
+    'O maior peixe do planeta é o tubarão baleia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vermelho em ingles',
+    'Vermelho em ingles é Red'
+
+)
+
+
+
